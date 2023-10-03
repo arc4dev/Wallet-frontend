f2207f57c1803daa3822a711b2a4a5100308b9e1
Adrian Miarczyński <adrian331144@gmail.com>
2023-10-04 00:57:14 +0200

    added pages

---
+++ src/components/App/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import css from './LoginPage.module.css';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className={css.loginPage}>LoginPage</div>
+    </>
+  );
+};
+export default LoginPage;
---
+++ src/components/App/Pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,11 @@
+import css from './RegistrationPage.module.css';
+
+const RegistrationPage = () => {
+  return (
+    <>
+      <div className={css.registrationPage}>RegistrationPage</div>
+    </>
+  );
+};
+
+export default RegistrationPage;
