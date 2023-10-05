f6544c4e1a227c366b1186faa70f0903218872a7
Adrian Miarczyński <adrian331144@gmail.com>
2023-10-05 17:24:09 +0200

    added currency and sylese

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,11 @@
+import Currency from 'components/Currency/Currency';
 import css from './App.module.css';
 
 export const App = () => {
-  return <div className={css.box}>Projekt Wallet :D</div>;
+  return (
+    <div className={css.box}>
+      Projekt Wallet :D
+      <Currency />
+    </div>
+  );
 };
