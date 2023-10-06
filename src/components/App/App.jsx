6c2d60ef9b9655af417e45941ff95150a3aab7e7
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-06 12:51:14 +0200

    general icons prettify

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import css from './App.module.css';
+import ButtonAddTransactions from 'components/ButtonAddTransactions/ButtonAddTransactions';
 
 export const App = () => {
-  return <div className={css.box}>Projekt Wallet :D </div>;
+  return (
+    <div className={css.box}>
+      Projekt Wallet :D
+      <ButtonAddTransactions />
+    </div>
+  );
 };
