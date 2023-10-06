8ac809b4a9e1e1a79783e11b259b2a6d92c0d04e
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-06 14:58:30 +0200

    fix/deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import css from './App.module.css';
-import Navigation from 'components/Navigation/Navigation';
 
 export const App = () => {
   return (
     <div>
-      <Navigation />
       <div className={css.box}>Projekt Wallet :D</div>
     </div>
   );
-
+};
