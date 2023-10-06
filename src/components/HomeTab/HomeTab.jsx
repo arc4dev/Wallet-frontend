068683e91f993a799bc14b4cbccac42387c53f17
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-06 16:28:53 +0200

    Routing structure

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,9 +1,11 @@
 import css from './HomeTab.module.css';
+import ButtonAddTransactions from 'components/ButtonAddTransactions/ButtonAddTransactions';
 
 const HomeTab = () => {
   return (
     <>
       <div className={css.homeTab}>HomeTab</div>
+      <ButtonAddTransactions />
     </>
   );
 };
