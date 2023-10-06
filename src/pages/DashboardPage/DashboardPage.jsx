5c9e89e6751a10e3194923f4dd549106a9cdcdf7
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-10-06 23:25:54 +0200

    “commit”

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -5,49 +5,48 @@ import Navigation from '../../components/Navigation/Navigation';
 
 import { Outlet } from 'react-router-dom';
 
+import css from './DashboardPage.module.css';
+import svg from '../../assets/icons/icons.svg';
+
 class DashboardPage extends Component {
   componentDidMount() {
     this.fetchBalance();
-    this.fetchChart();
-    this.fetchCurrency();
-    this.fetchDiagramTab();
     this.fetchHeader();
-    this.fetchModalAddTransaction();
     this.fetchNavigation();
-    this.fetchTable();
   }
 
   fetchBalance() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
-  }
-  fetchChart() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
-  }
-  fetchCurrency() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
-  }
-  fetchDiagramTab() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
+    fetch('/api/balance')
+      .then(response => response.json())
+      .then(data => {
+        //Tu można aktualizować stan aplikacji z pobranymi danymi
+        console.log('Stan balansu:', data);
+      })
+      .catch(error => {
+        console.error('Błąd podczas pobierania stanu balansu:', error);
+      });
   }
   fetchHeader() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
-  }
-  fetchModalAddTransaction() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
+    fetch('/api/header')
+      .then(response => response.json())
+      .then(data => {
+        //Tu można aktualizować stan aplikacji z pobranymi danymi
+        console.log('Dane nagłówka:', data);
+      })
+      .catch(error => {
+        console.error('Błąd podczas pobierania danych nagłówka:', error);
+      });
   }
   fetchNavigation() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
-  }
-  fetchTable() {
-    //dodać logikę do pobierania stanu balansu z serwera lub API
-    //używając fetch lub Axios
+    fetch('/api/navigation')
+      .then(response => response.json())
+      .then(data => {
+        //Tu można aktualizować stan aplikacji z pobranymi danymi
+        console.log('Dane nawigacji:', data);
+      })
+      .catch(error => {
+        console.error('Błąd podczas pobierania danych nawigacji:', error);
+      });
   }
 
   render() {
@@ -65,25 +64,51 @@ class DashboardPage extends Component {
               {matches.small && (
                 <div>
                   {/* <h2>Mobile View</h2> */}
+                  {/* Elementy SVG z odpowiednimi klasami */}
+                  <svg className={`${css.background} ${css.right}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-2`}></use>
+                  </svg>
+
+                  <svg className={`${css.background} ${css.left}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-1`}></use>
+                  </svg>
+
+                  <Outlet />
                   <Header />
                   <Navigation />
-                  <Outlet />
                 </div>
               )}
               {matches.medium && (
                 <div>
                   {/* <h2>Tablet View</h2> */}
+                  {/* Elementy SVG z odpowiednimi klasami */}
+                  <svg className={`${css.background} ${css.right}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-2`}></use>
+                  </svg>
+
+                  <svg className={`${css.background} ${css.left}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-1`}></use>
+                  </svg>
+                  <Outlet />
                   <Header />
                   <Navigation />
-                  <Outlet />
                 </div>
               )}
               {matches.large && (
                 <div>
                   {/* <h2>Desktop View</h2> */}
+                  {/* Elementy SVG z odpowiednimi klasami */}
+                  <svg className={`${css.background} ${css.right}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-2`}></use>
+                  </svg>
+
+                  <svg className={`${css.background} ${css.left}`}>
+                    <use xlinkHref={`${svg}#icon-Ellipse-1`}></use>
+                  </svg>
+
+                  <Outlet />
                   <Header />
                   <Navigation />
-                  <Outlet />
                 </div>
               )}
             </>
