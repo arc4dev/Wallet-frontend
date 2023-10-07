0c56e057578dd6ffa83e876059fc883eeed77a4d
Karwan87 <damianuszynski@gmail.com>
2023-10-07 17:51:11 +0200

    Update DiagramTab.jsx
    showing table with summary

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import css from './DiagramTab.module.css';
+import TransactionsList from './TransactionsList';
+import Table from 'components/Table/Table';
+import ChartComponent from 'components/Chart/Chart';
 
 const DiagramTab = () => {
+  const transactionsList = TransactionsList;
   return (
-    <>
-      <div className={css.diagramTab}>DiagramTab</div>
-    </>
+    <div className={css.diagramTab}>
+      Statistics
+      <Table transactionsList={TransactionsList} />
+      {/* <ChartComponent transactionsList={transactionsList} /> */}
+    </div>
   );
 };
 export default DiagramTab;
