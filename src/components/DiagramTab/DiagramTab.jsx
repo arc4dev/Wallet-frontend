dbc91148cb807a20c13e08fd48aa9b02d880cf73
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-08 16:18:00 +0200

    cnflict

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import transactionsList from './TransactionsList';
+import TransactionsList from './TransactionsList';
 import Table from 'components/Table/Table';
 import css from './DiagramTab.module.css';
 // import ChartComponent from 'components/Chart/Chart';
@@ -9,7 +9,7 @@ const DiagramTab = ({ categories, transactionsList }) => {
   return (
     <div className={css.diagramTab}>
       Statistics
-      <Table transactionsList={transactionsList} categories={categories} />
+      <Table transactionsList={TransactionsList} categories={categories} />
     </div>
   );
 };
