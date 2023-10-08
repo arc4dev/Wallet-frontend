6218e7fad5931a559800605d686dd68579705fe3
Adrian Lacheta <adiczq@gmail.com>
2023-10-08 11:41:55 +0200

    Update ButtonAddTransactions.jsx

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './ButtonAddTransactions.module.css';
 import svg from '../../assets/icons/icons.svg';
+import ModalAddTransaction from '../../components/ModalAddTransaction/ModalAddTransaction';
 
-const ButtonAddTransactions = ({ onClick }) => {
+const ButtonAddTransactions = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
   return (
-    <div className={css.ButtonAddTransactions} onClick={onClick}>
-      <svg className={css.AddIcon}>
-        <use xlinkHref={`${svg}#icon-plus`}></use>
-      </svg>
+    <div>
+      <div className={css.ButtonAddTransactions} onClick={handleOpenModal}>
+        <svg className={css.AddIcon}>
+          <use xlinkHref={`${svg}#icon-plus`}></use>
+        </svg>
+      </div>
+
+      {isModalOpen && <ModalAddTransaction setOpenModal={setIsModalOpen} />}
     </div>
   );
 };
