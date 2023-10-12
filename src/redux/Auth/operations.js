126b188cd7dfe6aff0eb6021055e70b3aa069c5a
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-12 17:01:07 +0200

    redux auth, protected route, private route, useAuth hoook

--- src/redux/Auth/operations.js
+++ src/redux/Auth/operations.js
@@ -1,10 +1,58 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3001/api';
+
+const setAuthToken = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthToken = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
 
 export const registerUser = createAsyncThunk('/auth/register', async (body, thunkAPI) => {
   try {
-    const res = body; // Fetch here
+    const res = await axios.post('/auth-sign-up', body);
 
     return res.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.message);
+  }
+});
+
+export const loginUser = createAsyncThunk('/auth/logIn', async (body, thunkAPI) => {
+  try {
+    const res = await axios.post('/auth/sign-in', body);
+
+    setAuthToken(res.token);
+
+    return res.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk('/auth/refresh', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const persistedToken = state.auth.token;
+
+  if (persistedToken === null) return thunkAPI.rejectWithValue('Unable to refresh user!');
+
+  try {
+    setAuthToken(persistedToken);
+
+    const res = await axios.get('/users/current');
+
+    return res.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.message);
+  }
+});
+
+export const logoutUser = createAsyncThunk('/auth/logOut', async (_, thunkAPI) => {
+  try {
+    clearAuthToken();
   } catch (err) {
     return thunkAPI.rejectWithValue(err.message);
   }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,32 @@ import { configureStore } from '@reduxjs/toolkit';
 import { financeReducer } from './finance/slice';
 import { authReducer } from './Auth/slice';
 import { globalReducer } from './global/slice';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const authPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['token'],
+};
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
-    auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
     finance: financeReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
